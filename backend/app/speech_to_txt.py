532c0afdb364a2502bae0796f440e1b0531b6af9
tylernsnow <tnsnow23@gmail.com>
2025-03-31 05:59:56 -0500

    Create speech_to_txt.py
    (draft of) speech to text command backend

---
+++ backend/app/speech_to_txt.py
@@ -0,0 +1,35 @@
+#Recieves input from audio_input.jsx in frontend, processes audio into text, navigates to file input
+#Note: switch to different API than google (Sphinx?)
+#Link to microphone button
+#User clicks button->activates this script
+from flask import Blueprint, jsonify, request
+import os
+import speech_recognition
+import base64
+
+bp = Blueprint("audio-input", __name__, url_prefix="/audio-input")
+
+rec = speech_recognition.Recognizer()
+#read&encode 
+def speech_to_text():   #Process audio file into text
+    try:
+        audio_text = request.files['audio']
+
+    except Exception as e:
+        return jsonify({'response': f'Error: {str(e)}'}), 500
+
+    #FIXME: make following integrate w/ actual features:
+    if rec.recognize_google(audio_text)=="algebra": #FIXME: change from google
+        #link to algebra route/click tab
+        #after linked, prompt image upload
+        print("algebra route")
+    elif rec.recognize_google(audio_text)=="geometry":  #FIXME: change from google
+        #link to geometry route/click tab
+        #after linked, prompt image upload
+        print("geometry route")
+    elif rec.recognize_google(audio_text)=="graph": #FIXME: change from google
+        #link to graph route/click tab
+        #after linked, prompt image upload
+        print("graph route")
+    else:
+        return jsonify({"error": "Unrecognized command"}), 400
