3a0ea52a67d8aefdc145f4b40eaddca5b4e088c0
tylernsnow <tnsnow23@gmail.com>
2025-04-15 13:34:07 -0500

    Update audio-input.jsx

--- frontend/audio-input.jsx
+++ frontend/audio-input.jsx
@@ -1,1 +1,6 @@
+//using react-speech-to-text, handle audio recording and UI navigation
+//Unlike audio-input branch, recording, processing, and navigation are all handled in frontend through React
 
+//TODO: add react-speech-to-text to requirements.txt
+//TODO: add handleAudioRecording from old branch
+//TODO: add UI nav wrapper/UI navigation functionality
